1695b4e368bb54398fc26f6717963dd4e249ece0
angelexi76 <angelasanchezbarbosa@gmail.com>
2023-04-19 20:35:45 -0500

    botones funcionales

--- app/src/main/java/com/angelasanchez/minegocio/Prod1Activity.kt
+++ app/src/main/java/com/angelasanchez/minegocio/Prod1Activity.kt
@@ -4,18 +4,23 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
+import android.view.MenuItem
 import android.view.View
 import android.view.View.OnClickListener
 import android.widget.Button
 import android.widget.TextView
+import android.widget.Toast
 import org.w3c.dom.Text
 
-private operator fun Int.plus(s: String) {
 
-}
+//private operator fun Int.plus(s: String) {
+
+//}//
+
+
 class Prod1Activity : AppCompatActivity() {
 
-    var contador: Int = 0
+    private var contador: Int = 0
     private lateinit var text: View
     private lateinit var btnsum: Button
     private lateinit var btnrest: Button
@@ -23,38 +28,43 @@ class Prod1Activity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        contador = 0
+
         setContentView(R.layout.activity_prod1)
+
         setSupportActionBar(findViewById(R.id.my_barra))
 
         btnsum = findViewById<Button>(R.id.btnsum)
-
-        btnsum.setOnClickListener {
-            OnClickListener() {
-                contador++
-                btnsum.text = contador.toString()
-            }
-        }
-
         btnrest = findViewById<Button>(R.id.btnrest)
-        btnrest.setOnClickListener {
-            OnClickListener() {
-                contador--
-            }
+        text=findViewById<Button>(R.id.text)
+
+
+
+
+
+
+    fun add1(){
+        btnsum.setOnClickListener {
         }
+        contador++
+        btnsum.text=contador.toString(contador)
 
-        text = findViewById<Button>(R.id.text)
+    }
 
+     fun subtract1(view: View) {
+         btnsum.setOnClickListener {
+         }
+         contador--
+         btnrest.text=contador.toString(contador)
+}
+
+
+    fun textcontador(){
         text.setOnClickListener {
-            OnClickListener() {
-
-                btnsum.text = (contador + "").toString()
-                btnrest.text = (contador + "").toString()
-            }
-
 
         }
 
+
+    }
 
     }
 
@@ -64,10 +74,28 @@ class Prod1Activity : AppCompatActivity() {
 
 
 
-
+//acciones del menu,sobreescribimos el metodo onCreateOptionsMenu que pide parametros de tipo menu y es bool
+//  menuInflater.inflate  convierte el xml en su representacion grafica
+    //retorna de la clase padre y el parametro es el menu
         override fun onCreateOptionsMenu(menu: Menu?): Boolean {
             menuInflater.inflate(R.menu.menu, menu)
             return super.onCreateOptionsMenu(menu)
         }
+//when evalua si el item q se pulso coincide con el  R.id.txv_action_search    si es true creamos la funcion anonima  -> q defina la accion a realizar
+         override fun onOptionsItemSelected(item: MenuItem): Boolean =when(item.itemId) {
+             R.id.txv_action_search-> {
+                 Toast.makeText(this,R.string.txv_action_search,Toast.LENGTH_LONG).show()
+                 true
+
+             }
+
+              R.id.txv_action_search-> {
+                    Toast.makeText(this, R.string.txv_action_logout, Toast.LENGTH_LONG).show()
+                    true
+                }
+                    else -> {
+                        super.onOptionsItemSelected(item)
+              }
+         }
 
 }
